c143e3044fb6d362d592bc6e6426a58ea946178a
Vaibhav Katariya <vbhr2000@gmail.com>
2020-06-05 01:53:10 +0530

    Project 3
    assigning, accessing and deleting elements/values/items for different data structure

---
+++ DataStruct.py
@@ -0,0 +1,18 @@
+# 1.assigning elements to different tupple:
+list1=[1,"two",(3,),{4},{5:"five"}]
+list2=list(input())
+list2.append(input())
+print(a+b) #or list1.extend(list2)
+
+# 2.accessimg elements from a tupple:
+tup="HELLO world"
+print(tup[0],tup[6:]) #output: H world
+
+# 3.deleting different dictionary elements:
+di={}
+di[1]="one"
+di[2]="two"
+di["hello"]="world"
+print(di)
+del di["hello"]
+print(di)
