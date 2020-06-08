d5f79d1e9db5dff392a55a0680a21ffff5c6680f
Vaibhav Katariya <vbhr2000@gmail.com>
2020-06-08 09:25:03 +0530

    Update DataStruct.py

--- DataStruct.py
+++ DataStruct.py
@@ -2,7 +2,7 @@
 list1=[1,"two",(3,),{4},{5:"five"}]
 list2=list(input())
 list2.append(input())
-print(a+b) #or list1.extend(list2)
+print(list1+list2) #or list1.extend(list2)
 
 # 2.accessimg elements from a tupple:
 tup="HELLO world"
